0ecc64c5a3aba76b25073622077ed1acf35ae584
Mayank-rj <mayank.kumar.rajoria@gmail.com>
2024-09-18 14:38:40 +0530

    first commit of parallax

---
+++ src/main.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Home from "./Pages/Home/Home.jsx"
+import About from "./Pages/About/About.jsx"
+import Contact from "./Pages/Contact/Contact.jsx"
+import ErrorPage from "./Pages/Error/ErrorPage.jsx";
+import "./index.css";
+
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store/store.js";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/home" />,
+      },
+      {
+        path: 'home',
+        element: <Home />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
